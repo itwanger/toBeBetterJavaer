134399421098061e50b0d5f5f990918d1c1f3df2
沉默王二 <www.qing_gee@163.com>
2018-12-13 17:06:55 +0800

    tijiao

--- java_demo/src/main/java/com/cmower/java_demo/App.java
+++ java_demo/src/main/java/com/cmower/java_demo/App.java
@@ -1,5 +1,8 @@
 package com.cmower.java_demo;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.print( "每个月给女儿存100，按照12%的月利率，现在需要存款：" );
+        
+        BigDecimal deposit_for_daughter = BigDecimal.valueOf(100);
+        
+        int month = 2 * 12 + 8;
+        for (int i = 1; i <= month; i++) {
+        		deposit_for_daughter = deposit_for_daughter.multiply(BigDecimal.valueOf(1.12));
+		}
+        System.out.println(deposit_for_daughter.setScale(2, RoundingMode.HALF_UP));
+        
+        System.out.println(NiubiUtil.getNextDay());
     }
 }
---
+++ java_demo/src/main/java/com/cmower/java_demo/Wanger.java
@@ -0,0 +1,15 @@
+package com.cmower.java_demo;
+
+public class Wanger {
+	
+	// 私生活
+	private String life;
+	
+	// 外在形象
+	public String image;
+	
+	// 被保护的财产
+	protected String money;
+	
+	String test;
+}
