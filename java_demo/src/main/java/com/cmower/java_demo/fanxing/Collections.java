db93faaf6815566c18b706ec0beaa66d5227e80f
qinggee <www.qing_gee@163.com>
2019-05-16 12:32:27 +0800

    更新

---
+++ java_demo/src/main/java/com/cmower/java_demo/fanxing/Collections.java
@@ -0,0 +1,8 @@
+package com.cmower.java_demo.fanxing;
+
+public class Collections {
+	public static <T> void copy(Arraylist<? super T> dest, Arraylist<? extends T> src) {
+		for (int i = 0; i < src.size(); i++)
+			dest.set(i, src.get(i));
+	}
+}
