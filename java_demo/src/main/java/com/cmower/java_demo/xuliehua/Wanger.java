3ccc4916029e61d1e3e93c9268aaa001ce8a814f
沉默王二 <www.qing_gee@163.com>
2019-06-06 06:55:56 +0800

    aa

---
+++ java_demo/src/main/java/com/cmower/java_demo/xuliehua/Wanger.java
@@ -0,0 +1,34 @@
+package com.cmower.java_demo.xuliehua;
+
+import java.io.Serializable;
+
+class Wanger implements Serializable {
+    private static final long serialVersionUID = -2095916884810199532L;
+
+    private String name;
+    private int age;
+
+    public static String pre = "沉默";
+    transient String meizi = "王三";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Wanger{" + "name=" + name + ",age=" + age + ",pre=" + pre + ",meizi=" + meizi + "}";
+    }
+}
