c5a081523c7e7be3b65f66161dd5160aaa8950b7
沉默王二 <www.qing_gee@163.com>
2019-08-03 17:23:45 +0800

    gengxin

---
+++ java_demo/src/main/java/com/cmower/java_demo/dongtaidaili/WriterImpl.java
@@ -0,0 +1,11 @@
+package com.cmower.java_demo.dongtaidaili;
+
+// 被代理类（具体实现抽象接口的类）
+public class WriterImpl implements Writer{
+
+    @Override
+    public void writeWords() {
+        System.out.println("写个锤子");
+    }
+
+}
