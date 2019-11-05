f67391b4f004e943770238a60d6321b507f09a5d
沉默王二 <www.qing_gee@163.com>
2019-11-06 05:42:52 +0800

    新一波学习

---
+++ java_demo/src/main/java/com/cmower/java_demo/thinkingjava/Xiaohua.java
@@ -0,0 +1,11 @@
+package com.cmower.java_demo.thinkingjava;
+
+class Xiaohua extends Laohua {
+    public void paint() {
+        System.out.println("人像画");
+    }
+    
+    public void hack() {
+        System.out.println("黑客");
+    }
+}
