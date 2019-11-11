4611315e461137aaab518e2c0b7c09357fbd94a9
沉默王二 <www.qing_gee@163.com>
2019-11-12 06:14:18 +0800

    Update Cmower.java

--- java_demo/src/main/java/com/cmower/java_demo/programcreek/Cmower.java
+++ java_demo/src/main/java/com/cmower/java_demo/programcreek/Cmower.java
@@ -3,8 +3,9 @@ package com.cmower.java_demo.programcreek;
 public class Cmower {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        String cmower = "沉默王二，一枚有趣的程序员";
+        cmower = cmower.substring(0, 4);
+        System.out.println();
     }
 
 }
