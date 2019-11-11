fe4326c5a5abb31e73753721b38f26cac89fcd4d
沉默王二 <www.qing_gee@163.com>
2019-11-12 06:33:47 +0800

    忽略不必要的文件

--- java_demo/src/main/java/com/cmower/java_demo/programcreek/Cmower.java
+++ java_demo/src/main/java/com/cmower/java_demo/programcreek/Cmower.java
@@ -5,7 +5,7 @@ public class Cmower {
     public static void main(String[] args) {
         String cmower = "沉默王二，一枚有趣的程序员";
         cmower = cmower.substring(0, 4);
-        System.out.println();
+        System.out.println(cmower);
     }
 
 }
