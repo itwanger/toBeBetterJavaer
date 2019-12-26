9da34558d35b0d1355d08d0d0025e72b69436e1d
沉默王二 <www.qing_gee@163.com>
2019-12-26 11:41:27 +0800

    打印对象

---
+++ java_demo/src/main/java/com/cmower/java_demo/stackoverflow/printObject/Test.java
@@ -0,0 +1,31 @@
+package com.cmower.java_demo.stackoverflow.printObject;
+
+import com.google.common.base.MoreObjects;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        Cmower cmower = new Cmower("沉默王二");
+        System.out.println(cmower);
+
+        Cmower[] cmowers = {new Cmower("沉默王二"), new Cmower("沉默王三")};
+        System.out.println(Arrays.toString(cmowers));
+
+        System.out.println(ToStringBuilder.reflectionToString(cmower));
+
+        System.out.println(MoreObjects.toStringHelper(cmower)
+                .add("name", cmower.getName())
+                .toString());
+
+        System.out.println(cmower);
+
+List<Cmower> list = new ArrayList<>();
+list.add(new Cmower("沉默王二"));
+list.add(new Cmower("沉默王三"));
+System.out.println(list);
+    }
+}
