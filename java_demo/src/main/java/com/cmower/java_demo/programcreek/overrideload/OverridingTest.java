c2fb7d4da2d913480a1fced2705e723f986eb710
沉默王二 <www.qing_gee@163.com>
2020-01-04 15:48:01 +0800

    教妹学Java

---
+++ java_demo/src/main/java/com/cmower/java_demo/programcreek/overrideload/OverridingTest.java
@@ -0,0 +1,8 @@
+package com.cmower.java_demo.programcreek.overrideload;
+
+public class OverridingTest {
+    public static void main(String[] args) {
+        LaoWang wang = new XiaoWang();
+        wang.write();
+    }
+}
