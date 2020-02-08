e40a1d9eb01d1402a1c1d1f7d58269fb31ef6d3b
沉默王二 <www.qing_gee@163.com>
2020-02-08 11:26:09 +0800

    Create Demo.java

---
+++ java_demo/src/main/java/com/cmower/java_demo/javatpoint/nine/Demo.java
@@ -0,0 +1,11 @@
+package com.cmower.java_demo.javatpoint.nine;
+
+public class Demo {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = -10;
+
+        short s = 10000;
+        short r = -5000;
+    }
+}
