f04c981c9150421b67809da76c67492145993a8c
沉默王二 <www.qing_gee@163.com>
2020-02-12 11:12:48 +0800

    Create String2int.java

---
+++ java_demo/src/main/java/com/cmower/java_demo/mkyoung/String2int.java
@@ -0,0 +1,17 @@
+package com.cmower.java_demo.mkyoung;
+
+public class String2int {
+    public static void main(String[] args) {
+//String number = "10";
+//int result = Integer.parseInt(number);
+//System.out.println(result);
+
+//String number = "10";
+//Integer result = Integer.valueOf(number);
+//System.out.println(result);
+
+String number = "10A";
+int result = Integer.parseInt(number);
+System.out.println(result);
+    }
+}
