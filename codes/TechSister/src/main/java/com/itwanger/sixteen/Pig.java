c0c380df41e3e8c9c2b2a89c3931aa31da8fd3aa
沉默王二 <www.qing_gee@163.com>
2020-11-20 08:14:54 +0800

    对象和类

---
+++ codes/TechSister/src/main/java/com/itwanger/sixteen/Pig.java
@@ -0,0 +1,15 @@
+package com.itwanger.sixteen;
+
+/**
+ * 微信搜索「沉默王二」，回复 Java
+ *
+ * @author 沉默王二
+ * @date 2020/11/19
+ */
+public class Pig {
+    private String color;
+
+    public void eat() {
+        System.out.println("吃");
+    }
+}
