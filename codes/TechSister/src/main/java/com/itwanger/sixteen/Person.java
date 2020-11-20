07e6d0b26456cd71c3a9ec7a4d61f1bf65877ad9
沉默王二 <www.qing_gee@163.com>
2020-11-20 08:14:54 +0800

    对象和类

---
+++ codes/TechSister/src/main/java/com/itwanger/sixteen/Person.java
@@ -0,0 +1,25 @@
+package com.itwanger.sixteen;
+
+/**
+ * 微信搜索「沉默王二」，回复 Java
+ *
+ * @author 沉默王二
+ * @date 2020/11/19
+ */
+public class Person {
+    private String name;
+    private int age;
+    private int sex;
+
+    private void eat() {
+
+    }
+
+    private void sleep() {
+
+    }
+
+    private void dadoudou() {
+
+    }
+}
