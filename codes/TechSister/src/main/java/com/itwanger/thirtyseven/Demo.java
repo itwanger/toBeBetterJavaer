cd6ee9534e8487b240d86f0510550d00a6ad58f1
沉默王二 <www.qing_gee@163.com>
2021-06-19 22:58:42 +0800

    代码

---
+++ codes/TechSister/src/main/java/com/itwanger/thirtyseven/Demo.java
@@ -0,0 +1,12 @@
+package com.itwanger.thirtyseven;
+
+/**
+ * @author 微信搜「沉默王二」，回复关键字 Java
+ */
+public class Demo {
+    public static void main(String[] args) {
+        String chenmo = "沉默";
+        String wanger = "王二";
+        System.out.println(chenmo + wanger);
+    }
+}
---
+++ codes/TechSister/src/main/java/com/itwanger/thirtysix/StringDemo.java
@@ -0,0 +1,11 @@
+package com.itwanger.thirtysix;
+
+
+/**
+ * @author 微信搜「沉默王二」，回复关键字 Java
+ */
+public class StringDemo {
+    public static void main(String[] args) {
+        System.out.println(new String("小萝莉").intern() == "小萝莉");
+    }
+}
