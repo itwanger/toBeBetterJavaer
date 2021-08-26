992d4a0d9d2cb0f9d4cfe3b3755c3a6421c2b8ab
itwanger <www.qing_gee@163.com>
2021-08-26 11:20:00 +0800

    Create Demo.java

---
+++ codes/java8demo/src/main/java/com/itwanger/s53/Demo.java
@@ -0,0 +1,17 @@
+package com.itwanger.s53;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * @author 微信搜「沉默王二」，回复关键字 Java
+ */
+public class Demo {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+
+        for (int i = 0; i < 16; i++) {
+            list.add("沉默王二");
+        }
+    }
+}
