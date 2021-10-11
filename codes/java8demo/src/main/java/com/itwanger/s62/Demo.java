63a0c13da1244628586d05328da7e608f4cd1f82
itwanger <www.qing_gee@163.com>
2021-10-11 10:05:39 +0800

    代码提交

---
+++ codes/java8demo/src/main/java/com/itwanger/s62/Demo.java
@@ -0,0 +1,35 @@
+package com.itwanger.s62;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author 微信搜「沉默王二」，回复关键字 Java
+ */
+public class Demo {
+    public static void main(String[] args) {
+//        List<Integer> list = new ArrayList<>();
+//        for (Integer num : list) {
+//            System.out.println(num);
+//        }
+
+        List<String> list = new ArrayList<>();
+        list.add("沉默王二");
+        list.add("沉默王三");
+        list.add("沉默王四");
+
+for (int i = 0; i < list.size(); i++) {
+    System.out.print(list.get(i) + "，");
+}
+
+Iterator it = list.iterator();
+while (it.hasNext()) {
+    System.out.print(it.next() + "，");
+}
+
+for (String str : list) {
+    System.out.print(str + "，");
+}
+    }
+}
