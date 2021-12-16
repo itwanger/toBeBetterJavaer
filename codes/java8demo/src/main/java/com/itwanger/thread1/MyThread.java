e5ec68f411fddb50e439e5e3ba9ff1a2fa0eb830
itwanger <www.qing_gee@163.com>
2021-12-16 11:18:47 +0800

    thread

---
+++ codes/java8demo/src/main/java/com/itwanger/thread1/MyThread.java
@@ -0,0 +1,28 @@
+package com.itwanger.thread1;
+
+/**
+ * @author 微信搜「沉默王二」，回复关键字 Java
+ */
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(getName() + ":打了" + i + "个小兵");
+        }
+    }
+
+    public static void main(String[] args) {
+//创建MyThread对象
+MyThread t1=new  MyThread();
+MyThread t2=new  MyThread();
+MyThread t3=new  MyThread();
+//设置线程的名字
+t1.setName("鲁班");
+t2.setName("刘备");
+t3.setName("亚瑟");
+//启动线程
+t1.start();
+t2.start();
+t3.start();
+    }
+}
