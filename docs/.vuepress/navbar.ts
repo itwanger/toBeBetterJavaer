cb653340fd737896017db15b414d5f94d5dc8524
itwanger <www.qing_gee@163.com>
2022-03-16 18:19:25 +0800

    结构优化

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -2,22 +2,22 @@ import { defineNavbarConfig } from "vuepress-theme-hope";
 
 export default defineNavbarConfig([
   { 
-    text: "Java进阶之路", 
+    text: "进阶之路", 
     icon: "lujing", 
     link: "/home.md" 
   },
   { 
-    text: "Java优质专栏", 
+    text: "优质专栏", 
     icon: "youzhi", 
     link: "/zhuanlan/java-mianshi-zhinan.md" 
   },
   {
-    text: "Java学习路线",
+    text: "学习路线",
     icon: "luxian",
     link: "/xuexiluxian/"
   },
   {
-    text: "Java常读书单下载",
+    text: "计算机经典书单下载",
     icon: "pdf",
     link: "/download/java.md",
   },
