72464dbdd51d0bfd1e8f679de93bbaa2d9961a71
itwanger <www.qing_gee@163.com>
2022-03-18 10:36:27 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -20,7 +20,7 @@ export default defineHopeConfig({
     "/": {
       lang: "zh-CN",
       title: "Java 程序员进阶之路",
-      description: "一份通俗易懂、风趣幽默的Java学习指南",
+      description: "一份通俗易懂、风趣幽默的Java学习指南，内容涵盖Java基础、Java并发编程、Java虚拟机、Java企业级开发、Java面试等核心知识点。学Java，就认准Java程序员进阶之路",
     },
   },
 
