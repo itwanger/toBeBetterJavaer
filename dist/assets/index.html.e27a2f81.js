e36a52c8b4ab898a76d7ecb26f83fc3b538afdb6
itwanger <www.qing_gee@163.com>
2022-11-19 08:08:13 +0800

    删除静态页面

--- dist/assets/index.html.e27a2f81.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as t,c}from"./app.99eb8281.js";const o={};function r(n,_){return t(),c("div")}const f=e(o,[["render",r],["__file","index.html.vue"]]);export{f as default};
