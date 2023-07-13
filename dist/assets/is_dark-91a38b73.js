6836b8028ab5dc7742ed7f7704bc542ed02360a5
itwanger <www.qing_gee@163.com>
2023-07-13 15:13:29 +0800

    2023年07月13日 tobebetterjavaer.com 的最后一版

---
+++ dist/assets/is_dark-91a38b73.js
@@ -0,0 +1,1 @@
+import{aG as c,aH as a}from"./mermaid.core-7e1b9814.js";const e=n=>{const{r,g:t,b:o}=c.parse(n),s=.2126*a.channel.toLinear(r)+.7152*a.channel.toLinear(t)+.0722*a.channel.toLinear(o);return a.lang.round(s)},i=e,l=n=>i(n)>=.5,u=l,h=n=>!u(n),L=h;export{L as i};
