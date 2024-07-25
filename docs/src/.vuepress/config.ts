a5951dd73425e20d8a0f471db4c38e12cc26d1c5
沉默王二 <www.qing_gee@163.com>
2024-07-25 17:46:09 +0800

    最新的 hope 打包

---
+++ docs/src/.vuepress/config.ts
@@ -0,0 +1,76 @@
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  // HTML 目录
+  dest: "./dist",
+
+  lang: "zh-CN",
+  // 标题
+  title: "二哥的Java进阶之路",
+  // 描述
+  description: "一份通俗易懂、风趣幽默的Java学习指南，内容涵盖Java基础、Java并发编程、Java虚拟机、Java企业级开发、Java面试等核心知识点。学Java，就认准二哥的Java进阶之路",
+
+  theme,
+
+  // pwa 建议设置为 false
+  shouldPrefetch: false,
+
+  head: [
+    // meta
+    ["meta", { name: "robots", content: "all" }],
+    ["meta", { name: "author", content: "沉默王二" }],
+    [
+      "meta",
+      {
+        "http-equiv": "Cache-Control",
+        content: "no-cache, no-store, must-revalidate",
+      },
+    ],
+    ["meta", { "http-equiv": "Pragma", content: "no-cache" }],
+    ["meta", { "http-equiv": "Expires", content: "0" }],
+    [
+      "meta",
+      {
+        name: "keywords",
+        content:
+          "Java, Java基础, 并发编程, JVM, 虚拟机, 数据库, MySQL, Spring, Redis, MyBatis, SpringBoot, IDEA, 求职面试, 面渣逆袭, 学习路线",
+      },
+    ],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "script",{},
+      `
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?5230ac143650bf5eb3c14f3fb9b1d3ec";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
+      `
+    ],
+    [
+      "script",{},
+      `
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?59aa453e7e706422c636c079fc1cb031";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
+      `
+    ],
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "//at.alicdn.com/t/font_3180624_7cy10l7jqqh.css",
+      },
+    ],
+  ],
+});
---
+++ docs/src/.vuepress/theme.ts
@@ -0,0 +1,270 @@
+import { hopeTheme } from "vuepress-theme-hope";
+
+import navbar from "./navbar.js";
+import sidebar from "./sidebar.js";
+
+export default hopeTheme({
+  hostname: "https://javabetter.cn",
+  // 网站图标
+
+  author: {
+    name: "沉默王二",
+    url: "/about-the-author/",
+  },
+
+  // 阿里妈妈图标的前缀
+  iconPrefix: "iconfont icon-",
+
+  logo: "https://cdn.tobebetterjavaer.com/tobebetterjavaer/images/logo-02.png",
+
+  // Git 仓库和编辑链接
+  repo: "https://github.com/itwanger/toBeBetterJavaer",
+  repoLabel: "GitHub",
+  docsDir: "docs",
+  // 以前的默认仓库分支名，方便提交 pr 和 issue
+  docsBranch: "master",
+  breadcrumb: false,
+
+  // 全屏按钮
+  fullscreen: true,
+  // 在深色模式，浅色模式和自动之间切换 (默认)
+  darkmode: "switch",
+
+  // 导航栏
+  navbar,
+
+  // 侧边栏
+  sidebar,
+
+  // 页脚支持
+  footer: '<a href="https://beian.miit.gov.cn/" target="_blank">豫ICP备2021038026号-4</a>'
+  +'<img src="https://cdn.tobebetterjavaer.com/tobebetterjavaer/images/beian.png" height="15px" width="15px" />'
+  +'<a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41030502000411">'
+  +'<span>豫公网安备 41030502000411号</span>'
+  +'</a>',
+  displayFooter: true,
+
+ // 加密
+ encrypt: {
+    config: {
+      // 这只会加密 config/page.html
+      "/nice-article/itmind/ideapjazjczxjhmzcmyjjhcxgxz.html": ["1110", "5210"],
+      "/nice-article/itmind/webstormjhmwebstormwdzsjhmxbxt.html": ["1110", "5210"],
+      "/nice-article/itmind/sublimetextzcmpjazjcqckyxbxt.html": ["1110", "5210"],
+    },
+  },
+  // 提示文字
+  encryptLocales: {
+    placeholder: "微信搜‘沉默王二’回复‘密码’获取口令",
+
+    /**
+     * Passwrod error hint
+     */
+    errorHint: "哈哈，别调戏人家啦，按规则来嘛",
+  },
+
+  // 多语言配置
+  metaLocales: {
+    editLink: "在 GitHub 上编辑此页",
+  },
+
+  // 文章信息，可以填入数组，数组的顺序是各条目显示的顺序
+  pageInfo: ["Author", "Original", "Date", "Category", "Tag", "Word","ReadingTime"],
+
+  blog: {
+    // 个人介绍页地址
+    intro: "/about-the-author/",
+    sidebarDisplay: "mobile",
+    // 博主头像
+    avatar: "/assets/icon/itwanger-282.png",
+    // 座右铭
+    description:"没有什么使我停留——除了目的，纵然岸旁有玫瑰、有绿荫、有宁静的港湾，我是不系之舟。",
+    medias: {
+      Zhihu: "https://www.zhihu.com/people/cmower",
+      CSDN: "https://blog.csdn.net/qing_gee",
+      Github: "https://github.com/itwanger",
+      Gitee: "https://gitee.com/itwanger",
+    },
+  },
+
+  // 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
+  // hotReload: true,
+
+  // 在这里配置主题提供的插件
+  plugins: {
+    blog: true,
+    // 注意: 仅用于测试! 你必须自行生成并在生产环境中使用自己的评论服务
+    comment: {
+      provider: "Giscus",
+      repo :"itwanger/tobebetterjavaer-giscus",
+      repoId:"R_kgDOHBJssg",
+      category:"Announcements",
+      categoryId:"DIC_kwDOHBJsss4COJOx",
+    },
+
+    components: {
+      components: ["Badge", "VPCard"],
+    },
+
+    // 此处开启了很多功能用于演示，你应仅保留用到的功能。
+    mdEnhance: {
+      align: true,
+      attrs: true,
+      codetabs: true,
+      component: true,
+      demo: true,
+      figure: true,
+      imgLazyload: true,
+      imgSize: true,
+      include: true,
+      mark: true,
+      plantuml: true,
+      spoiler: true,
+      stylize: [
+        {
+          matcher: "Recommended",
+          replacer: ({ tag }) => {
+            if (tag === "em")
+              return {
+                tag: "Badge",
+                attrs: { type: "tip" },
+                content: "Recommended",
+              };
+          },
+        },
+      ],
+      sub: true,
+      sup: true,
+      tabs: true,
+      tasklist: true,
+      vPre: true,
+
+      // 在启用之前安装 chart.js
+      // chart: true,
+
+      // insert component easily
+
+      // 在启用之前安装 echarts
+      // echarts: true,
+
+      // 在启用之前安装 flowchart.ts
+      // flowchart: true,
+
+      // gfm requires mathjax-full to provide tex support
+      // gfm: true,
+
+      // 在启用之前安装 katex
+      // katex: true,
+
+      // 在启用之前安装 mathjax-full
+      // mathjax: true,
+
+      // 在启用之前安装 mermaid
+      // mermaid: true,
+
+      // playground: {
+      //   presets: ["ts", "vue"],
+      // },
+
+      // 在启用之前安装 reveal.js
+      // revealJs: {
+      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
+      // },
+
+      // 在启用之前安装 @vue/repl
+      // vuePlayground: true,
+
+      // install sandpack-vue3 before enabling it
+      // sandpack: true,
+    },
+
+    docsearch: {
+      appId: "O566AMFNJH",
+      apiKey: "d9aebea8bd1a4f1e01201464bbab255f",
+      indexName: "tobebetterjavaer",
+      locales: {
+        "/": {
+          placeholder: "搜索文档",
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+              },
+            },
+          },
+        },
+      },
+    },
+
+    // 如果你需要 PWA。安装 @vuepress/plugin-pwa 并取消下方注释
+    pwa: {
+      favicon: "https://cdn.tobebetterjavaer.com/tobebetterjavaer/images/favicon.ico",
+      cacheHTML: true,
+      cacheImage: true,
+      appendBase: true,
+      apple: {
+        icon: "/assets/icon/apple-icon-152.png",
+        statusBarColor: "black",
+      },
+      msTile: {
+        image: "/assets/icon/ms-icon-144.png",
+        color: "#ffffff",
+      },
+      manifest: {
+        icons: [
+          {
+            src: "/assets/icon/chrome-mask-512.png",
+            sizes: "512x512",
+            purpose: "maskable",
+            type: "image/png",
+          },
+          {
+            src: "/assets/icon/chrome-mask-192.png",
+            sizes: "192x192",
+            purpose: "maskable",
+            type: "image/png",
+          },
+          {
+            src: "/assets/icon/chrome-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/assets/icon/chrome-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    },
+  },
+});
