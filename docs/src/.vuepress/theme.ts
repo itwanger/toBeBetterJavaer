ecc26ec75d116ce66d07b3b9c4760d8be2fd177e
Mister-Hope <mister-hope@outlook.com>
2024-12-16 00:42:44 +0800

    feat: bump deps

--- docs/src/.vuepress/theme.ts
+++ docs/src/.vuepress/theme.ts
@@ -37,39 +37,43 @@ export default hopeTheme({
   sidebar,
 
   // 页脚支持
-  footer: '<a href="https://beian.miit.gov.cn/" target="_blank">豫ICP备2021038026号-4</a>'
-  +'<img src="https://cdn.tobebetterjavaer.com/tobebetterjavaer/images/beian.png" height="15px" width="15px" />'
-  +'<a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41030502000411">'
-  +'<span>豫公网安备 41030502000411号</span>'
-  +'</a>',
+  footer: `\
+<a href="https://beian.miit.gov.cn/" target="_blank">
+  豫ICP备2021038026号-4
+</a>
+<img src="https://cdn.tobebetterjavaer.com/tobebetterjavaer/images/beian.png" height="15px" width="15px" />
+<a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41030502000411" target="_blank">
+  豫公网安备 41030502000411号
+</a>
+`,
   displayFooter: true,
 
- // 加密
- encrypt: {
+  // 加密
+  encrypt: {
     config: {
       // 这只会加密 config/page.html
       "/nice-article/itmind/ideapjazjczxjhmzcmyjjhcxgxz.html": ["1110", "5210"],
-      "/nice-article/itmind/webstormjhmwebstormwdzsjhmxbxt.html": ["1110", "5210"],
-      "/nice-article/itmind/sublimetextzcmpjazjcqckyxbxt.html": ["1110", "5210"],
+      "/nice-article/itmind/webstormjhmwebstormwdzsjhmxbxt.html": [
+        "1110",
+        "5210",
+      ],
+      "/nice-article/itmind/sublimetextzcmpjazjcqckyxbxt.html": [
+        "1110",
+        "5210",
+      ],
     },
-  },
-  // 提示文字
-  encryptLocales: {
-    placeholder: "微信搜‘沉默王二’回复‘密码’获取口令",
-
-    /**
-     * Passwrod error hint
-     */
-    errorHint: "哈哈，别调戏人家啦，按规则来嘛",
-  },
-
-  // 多语言配置
-  metaLocales: {
-    editLink: "在 GitHub 上编辑此页",
   },
 
   // 文章信息，可以填入数组，数组的顺序是各条目显示的顺序
-  pageInfo: ["Author", "Original", "Date", "Category", "Tag", "Word","ReadingTime"],
+  pageInfo: [
+    "Author",
+    "Original",
+    "Date",
+    "Category",
+    "Tag",
+    "Word",
+    "ReadingTime",
+  ],
 
   blog: {
     // 个人介绍页地址
@@ -78,7 +82,8 @@ export default hopeTheme({
     // 博主头像
     avatar: "/assets/icon/itwanger-282.png",
     // 座右铭
-    description:"没有什么使我停留——除了目的，纵然岸旁有玫瑰、有绿荫、有宁静的港湾，我是不系之舟。",
+    description:
+      "没有什么使我停留——除了目的，纵然岸旁有玫瑰、有绿荫、有宁静的港湾，我是不系之舟。",
     medias: {
       Zhihu: "https://www.zhihu.com/people/cmower",
       CSDN: "https://blog.csdn.net/qing_gee",
@@ -87,161 +92,55 @@ export default hopeTheme({
     },
   },
 
-  // 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
-  // hotReload: true,
+  // 多语言配置
+  metaLocales: {
+    editLink: "在 GitHub 上编辑此页",
+  },
 
-  // 在这里配置主题提供的插件
+  encryptLocales: {
+    placeholder: "微信搜‘沉默王二’回复‘密码’获取口令",
+    errorHint: "哈哈，别调戏人家啦，按规则来嘛",
+  },
+
+  markdown: {
+    // 启用 figure，为图像添加描述
+    figure: true,
+    // 启用图片懒加载
+    imgLazyload: true,
+    // 其用数学公式
+    math: true,
+  },
+
   plugins: {
     blog: true,
-    // 注意: 仅用于测试! 你必须自行生成并在生产环境中使用自己的评论服务
+
     comment: {
       provider: "Giscus",
-      repo :"itwanger/tobebetterjavaer-giscus",
-      repoId:"R_kgDOHBJssg",
-      category:"Announcements",
-      categoryId:"DIC_kwDOHBJsss4COJOx",
+      repo: "itwanger/tobebetterjavaer-giscus",
+      repoId: "R_kgDOHBJssg",
+      category: "Announcements",
+      categoryId: "DIC_kwDOHBJsss4COJOx",
     },
 
     components: {
       components: ["Badge", "VPCard"],
     },
 
-    // 此处开启了很多功能用于演示，你应仅保留用到的功能。
-    mdEnhance: {
-      align: true,
-      attrs: true,
-      codetabs: true,
-      component: true,
-      demo: true,
-      // 启用 figure，为图像添加描述
-      figure: true,
-      // 启用图片懒加载
-      imgLazyload: true,
-      // 启用图片大小
-      imgSize: true,
-      include: true,
-      mark: true,
-      plantuml: true,
-      spoiler: true,
-      stylize: [
-        {
-          matcher: "Recommended",
-          replacer: ({ tag }) => {
-            if (tag === "em")
-              return {
-                tag: "Badge",
-                attrs: { type: "tip" },
-                content: "Recommended",
-              };
-          },
-        },
-      ],
-      sub: true,
-      sup: true,
-      tabs: true,
-      // 支持任务列表
-      tasklist: true,
-      vPre: true,
-
-      // 在启用之前安装 chart.js
-      // chart: true,
-
-      // insert component easily
-
-      // 在启用之前安装 echarts
-      // echarts: true,
-
-      // 在启用之前安装 flowchart.ts
-      // flowchart: true,
-
-      // gfm requires mathjax-full to provide tex support
-      // gfm: true,
-
-      // 在启用之前安装 katex
-      // katex: true,
-
-      // 在启用之前安装 mathjax-full
-      mathjax: true,
-
-      // 在启用之前安装 mermaid
-      // mermaid: true,
-
-      // playground: {
-      //   presets: ["ts", "vue"],
-      // },
-
-      // 在启用之前安装 reveal.js
-      // revealJs: {
-      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-      // },
-
-      // 在启用之前安装 @vue/repl
-      // vuePlayground: true,
-
-      // install sandpack-vue3 before enabling it
-      // sandpack: true,
-    },
-
     docsearch: {
       appId: "O566AMFNJH",
       apiKey: "d9aebea8bd1a4f1e01201464bbab255f",
       indexName: "tobebetterjavaer",
-      locales: {
-        "/": {
-          placeholder: "搜索文档",
-          translations: {
-            button: {
-              buttonText: "搜索文档",
-              buttonAriaLabel: "搜索文档",
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: "清除查询条件",
-                resetButtonAriaLabel: "清除查询条件",
-                cancelButtonText: "取消",
-                cancelButtonAriaLabel: "取消",
-              },
-              startScreen: {
-                recentSearchesTitle: "搜索历史",
-                noRecentSearchesText: "没有搜索历史",
-                saveRecentSearchButtonTitle: "保存至搜索历史",
-                removeRecentSearchButtonTitle: "从搜索历史中移除",
-                favoriteSearchesTitle: "收藏",
-                removeFavoriteSearchButtonTitle: "从收藏中移除",
-              },
-              errorScreen: {
-                titleText: "无法获取结果",
-                helpText: "你可能需要检查你的网络连接",
-              },
-              footer: {
-                selectText: "选择",
-                navigateText: "切换",
-                closeText: "关闭",
-                searchByText: "搜索提供者",
-              },
-              noResultsScreen: {
-                noResultsText: "无法找到相关结果",
-                suggestedQueryText: "你可以尝试查询",
-              },
-            },
-          },
-        },
-      },
     },
 
     // 如果你需要 PWA。安装 @vuepress/plugin-pwa 并取消下方注释
     pwa: {
-      favicon: "https://cdn.tobebetterjavaer.com/tobebetterjavaer/images/favicon.ico",
+      favicon:
+        "https://cdn.tobebetterjavaer.com/tobebetterjavaer/images/favicon.ico",
       cacheHTML: true,
       cacheImage: true,
       appendBase: true,
       apple: {
         icon: "/assets/icon/apple-icon-152.png",
-        statusBarColor: "black",
-      },
-      msTile: {
-        image: "/assets/icon/ms-icon-144.png",
-        color: "#ffffff",
       },
       manifest: {
         icons: [
