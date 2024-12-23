10953577dbf1b498046c3d43652ad2510535fc2c
沉默王二 <www.qing_gee@163.com>
2024-12-23 10:09:24 +0800

    Java 基础篇面渣逆袭优化
    并增加定制化组件。

---
+++ docs/src/.vuepress/components/mznxqrcode.vue
@@ -0,0 +1,23 @@
+<template>
+    <div style="border: 1px solid #096dd9; padding: 20px; border-radius: 15px; background-color: #f0f8ff; max-width: 800px; margin: 0 auto; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+      <p>嗨嗨嗨，时隔两年，面渣逆袭<a href="https://javabetter.cn/sidebar/sanfene/nixi.html" style="color: #096dd9; text-decoration: none; font-weight: bold;">第二版 PDF 终于可以下载了</a>。我们做了大量的优化：</p>
+    
+      <ol style="margin-left: 20px;">
+        <li><strong><span style="color:#d32f2f;">对于高频题</span></strong>：会标注在《<a href="https://javabetter.cn/zhishixingqiu/mianshi.html" style="color: #096dd9;">Java 面试指南（付费）</a>中出现的位置，哪家公司，原题是什么；如果你想节省时间的话，可以优先背诵这些题目，尽快做到知彼知己，百战不殆。</li>
+    
+        <li><strong><span style="color:#d32f2f;">结合项目</span></strong>：包括<a href="https://javabetter.cn/zhishixingqiu/paicoding.html" style="color: #096dd9;">技术派</a>、<a href="https://t.zsxq.com/0bhcI0Gs6" style="color: #096dd9;">mydb</a>、<a href="https://javabetter.cn/zhishixingqiu/pmhub.html" style="color: #096dd9;">pmhub</a>来组织语言，让面试官最大程度感受到你的诚意，而不是机械化的背诵。</li>
+    
+        <li><strong><span style="color:#d32f2f;">修复问题</span></strong>：第一版中出现的问题，包括球友们的私信反馈，网站留言区的评论，以及<a href="https://github.com/itwanger/toBeBetterJavaer/issues" style="color: #096dd9;"> GitHub 仓库中的 issue</a>，让这份面试指南更加完善。</li>
+    
+        <li><strong><span style="color:#d32f2f;">优化排版</span></strong>：增加手绘图，重新组织答案，使其更加口语化，从而更贴近面试官的预期。</li>
+      </ol>
+    
+      <p>你可以扫下面的二维码（或者长按自动识别）关注【<b>沉默王二</b>】公众号，发送关键字 <b>222</b> 来获取 PDF 版本，如果面渣逆袭真的对你有帮助，希望能给二哥的公众号加一个星标，满足我那一丁点虚荣心，这将是我更新下去的最强动力。</p>
+    
+      <div style="text-align: center; margin: 20px 0;">
+        <img src="https://cdn.tobebetterjavaer.com/tobebetterjavaer/images/gongzhonghao.png" alt="微信扫码或者长按识别，或者微信搜索“沉默王二”" style="max-width: 100%; height: auto;  border-radius: 10px;" />
+      </div>
+    
+      <p>面渣逆袭的整理工作真的太不容易了，花了我好多好多的时间和精力，内容完全免费，但质量却有口皆碑，就是为了做一点真正有意义的、纯粹的事情。</p>
+    </div>
+    </template>
